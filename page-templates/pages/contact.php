8a9f2024981e8e6dfc904da665f6e761dffaeb45
ozaki98 <k.ozaki@insel-art.jp>
2024-12-13 17:51:36 +0900

    contactform7追加

--- page-templates/pages/contact.php
+++ page-templates/pages/contact.php
@@ -3,5 +3,11 @@
     <div class="container">
         <h2>お問い合わせ</h2>
     </div>
+
+    <div class="contact-form">
+        <?php
+            echo do_shortcode('[contact-form-7 id="2fe2071" title="お問い合わせ"]'); 
+        ?>
+    </div>
 </main>
 <?php get_footer(); ?>
