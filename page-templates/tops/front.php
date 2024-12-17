db467df3096b842449f4eda191bd77874bf15fb6
higashida <higashida1227rei@gmail.com>
2024-12-17 21:08:19 +0900

    TOP　調整

--- page-templates/tops/front.php
+++ page-templates/tops/front.php
@@ -5,13 +5,22 @@
     <div class="swiper">
       <div class="swiper-wrapper">
         <div class="swiper-slide">
-          <img src="<?php echo get_template_directory_uri() ?>/assets/images/top/mv.png" alt="">
+          <picture>
+            <source media="(max-width:767px)" type="image/png" srcset="<?php echo get_template_directory_uri() ?>/assets/images/top/mv_sp.png" />
+            <img src="<?php echo get_template_directory_uri() ?>/assets/images/top/mv.png" alt="">
+          </picture>
         </div>
         <div class="swiper-slide">
-          <img src="<?php echo get_template_directory_uri() ?>/assets/images/top/mv.png" alt="">
+          <picture>
+            <source media="(max-width:767px)" type="image/png" srcset="<?php echo get_template_directory_uri() ?>/assets/images/top/mv_sp.png" />
+            <img src="<?php echo get_template_directory_uri() ?>/assets/images/top/mv.png" alt="">
+          </picture>
         </div>
         <div class="swiper-slide">
-          <img src="<?php echo get_template_directory_uri() ?>/assets/images/top/mv.png" alt="">
+          <picture>
+            <source media="(max-width:767px)" type="image/png" srcset="<?php echo get_template_directory_uri() ?>/assets/images/top/mv_sp.png" />
+            <img src="<?php echo get_template_directory_uri() ?>/assets/images/top/mv.png" alt="">
+          </picture>
         </div>
       </div>
     </div>
@@ -25,8 +34,8 @@
 
   <section class="about">
     <div class="l-inner">
-      <h2 class="heading-A"><span>ABOUT US</span>人と心と時間を満たす</h2>
-      <p class="about__txt">時に癒やし、時に賑わいの中心となり、人と人、心や時間までもが<br>満ちるお茶のある毎日のために。</p>
+      <h2 class="heading-A"><span>ABOUT US</span>人と心と時間を<br class="is-Sp">満たす</h2>
+      <p class="about__txt">時に癒やし、時に賑わいの中心となり、人と人、心や時間までもが<br class="is-Pc_Tab">満ちるお茶のある毎日のために。</p>
       <div class="btn-A">
         <a href=""><span>VIEW MORE</span></a>
       </div>
@@ -40,16 +49,19 @@
             <div class="strength__txt-area">
               <p class="strength__num">STRENGTH 01</p>
               <h3 class="strength__ttl">自社輸入</h3>
-              <p class="strength__txt">自社輸入だからできる。<br>
-                現地農園・工場と連携し、徹底した品質管理の元、<br>
+              <p class="strength__txt">自社輸入だからできる。<br class="is-Pc_Tab">
+                現地農園・工場と連携し、徹底した品質管理の元、<br class="is-Pc_Tab">
                 数百種の茶葉を取り扱っております。
               </p>
-              <div class="btn-A btn-A--white">
+              <div class="btn-A btn-A--white is-Pc_Tab">
                 <a href=""><span>VIEW MORE</span></a>
               </div>
             </div>
             <div class="strength__img-area">
               <img src="<?php echo get_template_directory_uri() ?>/assets/images/top/strength-img01.png" alt="">
+            </div>
+            <div class="btn-A btn-A--white is-Sp">
+              <a href=""><span>VIEW MORE</span></a>
             </div>
           </li>
           <li>
@@ -58,12 +70,15 @@
               <h3 class="strength__ttl">スペシャリスト</h3>
               <p class="strength__txt">明山茶業にはお茶のスペシャリスト、中国政府公認の高級評茶員が在籍しております。
               </p>
-              <div class="btn-A btn-A--white">
+              <div class="btn-A btn-A--white is-Pc_Tab">
                 <a href=""><span>VIEW MORE</span></a>
               </div>
             </div>
             <div class="strength__img-area">
               <img src="<?php echo get_template_directory_uri() ?>/assets/images/top/strength-img02.png" alt="">
+            </div>
+            <div class="btn-A btn-A--white is-Sp">
+              <a href=""><span>VIEW MORE</span></a>
             </div>
           </li>
           <li>
@@ -73,12 +88,15 @@
               <p class="strength__txt">
                 ＜OEM＞様々なご要望に応えるオリジナルティーをご提案します。
               </p>
-              <div class="btn-A btn-A--white">
+              <div class="btn-A btn-A--white is-Pc_Tab">
                 <a href=""><span>VIEW MORE</span></a>
               </div>
             </div>
             <div class="strength__img-area">
               <img src="<?php echo get_template_directory_uri() ?>/assets/images/top/strength-img03.png" alt="">
+            </div>
+            <div class="btn-A btn-A--white is-Sp">
+              <a href=""><span>VIEW MORE</span></a>
             </div>
           </li>
         </ul>
@@ -156,7 +174,7 @@
     <div class="news__container">
       <div class="news__ttl-area">
         <h2 class="heading-A"><span>NEWS</span>新着情報</h2>
-        <div class="btn-A">
+        <div class="btn-A is-Pc_Tab">
           <a href=""><span>VIEW MORE</span></a>
         </div>
       </div>
@@ -196,6 +214,9 @@
         </li>
         <?php endif; ?>
       </ul>
+      <div class="btn-A is-Sp">
+        <a href=""><span>VIEW MORE</span></a>
+      </div>
     </div>
   </section>
 </main>
