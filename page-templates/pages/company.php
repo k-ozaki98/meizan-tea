39ec65a3e23f82e67ec128b5cfb0494950718291
higashida <higashida1227rei@gmail.com>
2024-12-22 00:57:57 +0900

    会社案内　追加

--- page-templates/pages/company.php
+++ page-templates/pages/company.php
@@ -14,6 +14,51 @@
         <dd>1981年（昭和56年）6月</dd>
         <dt>所在地</dt>
         <dd>＜本社・倉庫＞<br>〒160-0022　東京都新宿区新宿1-25-11<br>＜営業オフィス＞<br>〒160-0022　東京都新宿区新宿1-29-16-201（東京メトロ丸の内線 新宿御苑駅 徒歩3分）<br>TEL: 03-3351-3240　FAX: 03-3351-3242　E-mail: info@meizan-tea.co.jp</dd>
+        <dt>資本金</dt>
+        <dd>3,000万円</dd>
+        <dt>事業内容</dt>
+        <dd>各種茶類輸入卸・業務用茶類加工</dd>
+        <dt>取扱品</dt>
+        <dd>中国茶・紅茶・ハーブティー・日本茶・健康茶・茶器等</dd>
+        <dt>役員</dt>
+        <dd><span class="dd-flex"><span>取締役会長</span>砂押　雅夫</span><br>
+          <span class="dd-flex"><span>代表取締役</span>砂押　悠子（伊藤　悠子）</span><br>
+          <span class="dd-flex"><span>取締役中国室長</span>張　文昕</span><br>
+          <span class="dd-flex"><span>取締役営業部長</span>石渡　真人</span><br>
+          <span class="dd-flex"><span>監査役</span>砂押　真樹子</span><br>
+          <span class="dd-flex"><span>顧問</span>高　銀塗</span><br>
+        </dd>
+        <dt>取引銀行</dt>
+        <dd>三菱UFJ銀行新宿支店<br>
+          三菱UFJ銀行西荻窪駅前支店<br>
+          みずほ銀行新宿中央支店<br>
+          三井住友銀行新宿通り支店<br>
+        </dd>
+        <dt>販売代理店</dt>
+        <dd>アサヒ飲料株式会社<br>
+          ハリオグラス株式会社
+        </dd>
+        <dt>主要取引先</dt>
+        <dd>各種食材問屋、茶関連専門店、飲料メーカー、デパート・小売チエーン店、
+          全国主要ホテル、中国料理専門店、各種レストランチエーン・カフェ等
+        </dd>
+        <dt>加入団体</dt>
+        <dd>首都圏業務用食品卸協同組合<br>
+          社団法人日本外食品卸協会<br>
+          公益社団法人日本中国料理協会<br>
+          東京都茶協同組合<br>
+          日本中国茶普及協会<br>
+          中国茶葉学会（中国・浙江省）<br>
+          台湾區製茶工業同業公會（台湾）<br>
+        </dd>
+        <dt>茶関連資格保持者 </dt>
+        <dd><span class="dd-flex">中国政府公認評茶員　　張　文昕</span><br>
+          <span class="dd-flex">中国政府公認評茶員　　砂押　真樹子</span><br>
+          <span class="dd-flex">中国政府公認評茶員　　金丸　隆</span><br>
+          <span class="dd-flex">日本中国茶普及協会インストラクター　　砂押　悠子</span><br>
+          <span class="dd-flex">日本中国茶普及協会インストラクター　　清水　浩義</span><br>
+          <span class="dd-flex">日本紅茶協会ティーインストラクター　　清水　浩義</span><br>
+        </dd>
       </dl>
     </section>
 
@@ -21,10 +66,187 @@
       <h3 class="company__ttl">沿革</h3>
 
       <dl class="history__wrap">
-        <dt><span>1981年</span></dt>
-        <dd>6月　明山茶業株式会社　現取締役会長 砂押 雅夫が東京都新宿区富久町にて設立</dd>
-        <dt><span>1987年</span></dt>
-        <dd>FOODEX JAPAN　国際食品・飲料展（幕張メッセ）に参加以降、現在も継続出展中</dd>
+        <div class="history__list">
+          <div class="history__list-data">
+            <span class="history__list-deco"></span>
+            <dt><span>1981年</span></dt>
+          </div>
+          <dd><span class="dd-flex"><span>6月　</span>明山茶業株式会社　現取締役会長 砂押 雅夫が東京都新宿区富久町にて設立</span></dd>
+        </div>
+
+        <div class="history__list">
+          <div class="history__list-data">
+            <span class="history__list-deco"></span>
+            <dt><span>1987年</span></dt>
+          </div>
+          <dd>FOODEX JAPAN　国際食品・飲料展（幕張メッセ）に参加以降、現在も継続出展中</dd>
+        </div>
+
+        <div class="history__list">
+          <div class="history__list-data">
+            <span class="history__list-deco"></span>
+            <dt><span>1988年</span></dt>
+          </div>
+          <dd>日本初、台湾茶原材料の缶飲料「赤ウーロン茶」<br>缶・ペットボトル・ティーバッグをAビール会社で発売（原料担当）</dd>
+        </div>
+
+        <div class="history__list">
+          <div class="history__list-data">
+            <span class="history__list-deco"></span>
+            <dt><span>1988年</span></dt>
+          </div>
+          <dd>日本初、台湾茶原材料の缶飲料「赤ウーロン茶」<br>缶・ペットボトル・ティーバッグをAビール会社で発売（原料担当）</dd>
+        </div>
+
+        <div class="history__list">
+          <div class="history__list-data">
+            <span class="history__list-deco"></span>
+            <dt><span>1989年</span></dt>
+          </div>
+          <dd><span class="dd-flex"><span>4月　</span>台湾區製茶工業同業公會より台湾茶の輸出貢献賞を受賞（「紅ウーロン茶」の原料供給により台湾茶業に貢献）</span></dd>
+        </div>
+
+        <div class="history__list">
+          <div class="history__list-data">
+            <span class="history__list-deco"></span>
+            <dt><span>1990年</span></dt>
+          </div>
+          <div class="history__list-dd">
+            <dd><span class="dd-flex"><span>4月　</span>首都圏業務用食品卸協同組合に加盟</span></dd>
+            <dd><span class="dd-flex"><span>9月　</span>インド・セイロン紅茶を本格的に取扱い開始</span></dd>
+            <dd><span class="dd-flex"><span>9月　</span>旅行代理店と香港お土産品に中国茶を契約</span></dd>
+          </div>
+        </div>
+
+        <div class="history__list">
+          <div class="history__list-data">
+            <span class="history__list-deco"></span>
+            <dt><span>1992年</span></dt>
+          </div>
+          <dd>東京都新宿区新宿　現住所へ移転</dd>
+        </div>
+
+        <div class="history__list">
+          <div class="history__list-data">
+            <span class="history__list-deco"></span>
+            <dt><span>1993年</span></dt>
+          </div>
+          <dd>甜茶取り扱い開始　1996年に花粉症対策としてヒット商品に<br>K食品で「ゆうゆう（悠 悠）茶」缶を発売（烏龍茶原料納入）</dd>
+        </div>
+
+        <div class="history__list">
+          <div class="history__list-data">
+            <span class="history__list-deco"></span>
+            <dt><span>1995年</span></dt>
+          </div>
+          <dd>福建省、広東省より中国茶の取扱いを本格化</dd>
+        </div>
+
+        <div class="history__list">
+          <div class="history__list-data">
+            <span class="history__list-deco"></span>
+            <dt><span>1997年</span></dt>
+          </div>
+          <dd>中国大陸よりコンテナ輸入開始<br>福建省のジャスミン茶加工工場と専属契約を結ぶ</dd>
+        </div>
+
+        <div class="history__list">
+          <div class="history__list-data">
+            <span class="history__list-deco"></span>
+            <dt><span>1999年</span></dt>
+          </div>
+          <dd>インターネットの普及に伴い全国通販業者への納入が増大</dd>
+        </div>
+
+        <div class="history__list">
+          <div class="history__list-data">
+            <span class="history__list-deco"></span>
+            <dt><span>2000年</span></dt>
+          </div>
+          <dd>社団法人 日本中国料理協会加盟</dd>
+        </div>
+
+        <div class="history__list">
+          <div class="history__list-data">
+            <span class="history__list-deco"></span>
+            <dt><span>2001年</span></dt>
+          </div>
+          <dd><span class="dd-flex"><span>4月　</span>Hビバレッジ社の「烏龍茶」壜の原料納入開始</span></dd>
+        </div>
+
+        <div class="history__list">
+          <div class="history__list-data">
+            <span class="history__list-deco"></span>
+            <dt><span>2003年～</span></dt>
+          </div>
+          <dd>FOODS Info Mart導入</dd>
+        </div>
+
+        <div class="history__list">
+          <div class="history__list-data">
+            <span class="history__list-deco"></span>
+            <dt><span>2004年</span></dt>
+          </div>
+          <div class="history__list-dd">
+            <dd><span class="dd-flex"><span>3月　</span>中国茶葉学会の正式団体会員に加入</span></dd>
+            <dd><span class="dd-flex"><span>7月　</span>東京農大（東京都世田谷区）オープンカレッジにて「中国茶講座」開講<br>2011年6月迄7年間担当　講師： 張 文昕　砂押真樹子</span></dd>
+          </div>
+        </div>
+
+        <div class="history__list">
+          <div class="history__list-data">
+            <span class="history__list-deco"></span>
+            <dt><span>2005年</span></dt>
+          </div>
+          <div class="history__list-dd">
+            <dd><span class="dd-flex"><span>5月　</span>日本中国茶普及協会に加盟</span></dd>
+            <dd><span class="dd-flex"><span>6月　</span>社団法人日本中国料理協会「圓卓」誌「会社紹介」に掲載される</span></dd>
+            <dd><span class="dd-flex"><span>11月　</span>和・イタリアン・アジアン・ハワイアン他、カフェやグローバルレストラン系列での紅茶、抹茶、ハーブティーの需要が増える</span></dd>
+          </div>
+        </div>
+
+        <div class="history__list">
+          <div class="history__list-data">
+            <span class="history__list-deco"></span>
+            <dt><span>2006年</span></dt>
+          </div>
+          <div class="history__list-dd">
+            <dd>西荻地域区民センター運営協議会（東京都杉並区）主催</dd>
+          </div>
+        </div>
+
+        <div class="history__list">
+          <div class="history__list-data">
+            <span class="history__list-deco"></span>
+            <dt><span>2008年～<br>2009年</span></dt>
+          </div>
+          <div class="history__list-dd">
+            <dd>「優雅に紅茶レッスン」「はじめての中国茶」講座を担当</dd>
+          </div>
+        </div>
+
+        <div class="history__list">
+          <div class="history__list-data">
+            <span class="history__list-deco"></span>
+            <dt><span>2010年</span></dt>
+          </div>
+          <div class="history__list-dd">
+            <dd><span class="dd-flex"><span>6月　</span>内閣府、佐賀県主催「第5回食育推進全国大会2010」にて中国茶講座とブース展示を担当福建省茶葉公司と契約、輸入開始</span></dd>
+            <dd>(株)総合食品研究所 月刊「 フードライフ」誌に取材協力<br>「今、静かに広がる中国茶ブーム」掲載</dd>
+            <dd>社団法人日本中国料理協会発行「圓卓」誌に「中国茶の真髄」隔月連載開始</dd>
+          </div>
+        </div>
+
+        <div class="history__list">
+          <div class="history__list-data">
+            <span class="history__list-deco"></span>
+            <dt><span>2011年</span></dt>
+          </div>
+          <div class="history__list-dd">
+            <dd>「中国茶の真髄」<a target="_blank" href="http://www.gourmet-china.info/">http://www.gourmet-china.info/</a></dd>
+          </div>
+        </div>
+
       </dl>
     </section>
 
