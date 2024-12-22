ae64660832078c9987bec63bc1eb2bb9c35226d4
higashida <higashida1227rei@gmail.com>
2024-12-22 22:54:05 +0900

    add oem

---
+++ page-templates/pages/oem.php
@@ -0,0 +1,63 @@
+<?php get_header('', ['pageId' => 'about']); ?>
+
+<main class="contents contents--oem">
+  <div class="l-inner">
+    <h2 class="heading-A"><span>original equipment manufacturing</span>OEM</h2>
+
+    <div class="oem-mv">
+      <div class="oem-mv__img">
+        <picture>
+          <source media="(max-width:767px)" type="image/png" srcset="<?php echo get_template_directory_uri() ?>/assets/images/oem/oem-mv_sp.png" />
+          <img src="<?php echo get_template_directory_uri() ?>/assets/images/oem/oem-mv.png" alt="">
+        </picture>
+      </div>
+    </div>
+
+    <section class="oem">
+      <h2 class="oem__ttl">専門知識を持ったスペシャリストが<br class="is-Pc_Tab">様々なご要望に<br class="is-Sp">沿ったオリジナルティーをご提案します。</h2>
+      <div class="oem__wrap">
+        <div class="oem__txt-area">
+          <p class="oem__txt">OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。</p>
+          <p class="oem__txt">OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。</p>
+        </div>
+        <div class="oem__img">
+          <img src="<?php echo get_template_directory_uri() ?>/assets/images/oem/oem-img01.png" alt="">
+        </div>
+      </div>
+      <p class="oem__txt">OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。</p>
+      <p class="oem__txt">OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。OEMの詳しい説明が入ります。</p>
+
+      <ul class="oem__list">
+        <li>カフェ、レストラン、ホテル、茶葉専門店などのメニューのご相談をお受けします。</li>
+        <li>企業や学校での中国茶・紅茶の研修や講座の企画、出張をいたします。</li>
+      </ul>
+
+      <div class="oem__img-wrap">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/images/oem/oem-img02.png" alt="">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/images/oem/oem-img03.png" alt="">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/images/oem/oem-img04.png" alt="">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/images/oem/oem-img05.png" alt="">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/images/oem/oem-img06.png" alt="">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/images/oem/oem-img07.png" alt="">
+      </div>
+
+    </section>
+  </div>
+  <div class="l-inner">
+    <div class="oem__contact">
+      <div class="oem__contact-icon">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/images/common/footer-icon.svg" alt="">
+      </div>
+      <div class="oem__contact-wrap">
+        <p class="oem__contact-ttl">おいしい・高品質・安全・安心なお茶を提供します</p>
+        <p class="oem__contact-txt">弊社では、輸出国での厳重農薬検査のほか、輸入時にも国内検査機関において残農薬検査を行っております。</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="btn-C">
+    <a href="/contact/">お問い合わせはこちら</a>
+  </div>
+
+</main>
+<?php get_footer(); ?>
