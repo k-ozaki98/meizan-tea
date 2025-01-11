c98d505de6aa5c270a4c9afa7ee4e5f700e0e0e5
higashida <higashida1227rei@gmail.com>
2025-01-12 00:13:12 +0900

    btn クラス追加

--- template-parts/chinese-other-tea.php
+++ template-parts/chinese-other-tea.php
@@ -1,6 +1,5 @@
-
 <div class="l-inner">
-    <div class="u-align-c btn-C">
-      <a href="../other-tea/">その他の「茶外茶」はこちら</a>
-    </div>
+  <div class="u-align-c product-btn btn-C">
+    <a href="../other-tea/">その他の「茶外茶」はこちら</a>
+  </div>
 </div>
